d5d8ff3dfa9aa1d360536f1dd17e2f2575e6798a
Parth Mistry <imparthmistry@gmail.com>
2020-06-27 12:09:00 +0530

    created flask api

---
+++ FlaskAPI/sss.py
@@ -0,0 +1,11 @@
+
+import requests
+from data_input import data_in
+
+URL = 'http://127.0.0.1:5000/predict'
+headers = {"Content-Type": "application/json"}
+data = {"input": data_in}
+
+r = requests.get(URL,headers=headers, json=data) 
+
+print(r.json())
